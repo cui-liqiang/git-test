2fc46b911f60ef4f98244d2a1217102837ffc2df
Hongmin Wang <whunmr@gmail.com>
2012-08-07 17:03:57 +0800

    add AppTest.java

--- src/test/java/com/git/AppTest.java
+++ src/test/java/com/git/AppTest.java
@@ -2,10 +2,14 @@ package com.git;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class AppTest {
     @Test
     public void testName() {
         assertEquals("name", new App().getName());
+
+        assertTrue(false);
     }
+
 }
